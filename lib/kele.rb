3b250aaf6b9f1ffa1b344692e1682b03e5f59b2f
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-06-28 17:01:12 +0100

    Mentor availability completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,18 +5,20 @@ class Kele
   include HTTParty
 
   def initialize(email, password)
-    # @bloc_api = "https://www.bloc.io/api/v1/"
     response = self.class.post(api_url + "sessions", body: { "email": email, "password": password })
-    # raise "Invalid email or password" if response.code == 404
-    raise "Return code: " + response.code.to_s if response.code != 200
+    raise "Invalid email or password" if response.code != 200
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    # @bloc_api = "https://www.bloc.io/api/v1/"
     response = self.class.get(api_url + "users/me", headers: { "authorization" => @auth_token })
 
     @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url + "mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
+    @mentor_availability = JSON.parse(response.body)
   end
 
 private
