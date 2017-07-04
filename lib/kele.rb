0f00587d4d9ad71b16ac7044407cb01627878bea
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-07-04 18:27:00 +0100

    Roadmap and checkpoint added

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require "httparty"
 require "json"
+# require "roadmap"
 
 class Kele
   include HTTParty
+  # include Roadmap
 
   def initialize(email, password)
     response = self.class.post(api_url + "sessions", body: { "email": email, "password": password })
@@ -12,13 +14,22 @@ class Kele
 
   def get_me
     response = self.class.get(api_url + "users/me", headers: { "authorization" => @auth_token })
-
     @user_data = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
     response = self.class.get(api_url + "mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
     @mentor_availability = JSON.parse(response.body)
+  end
+
+  def get_roadmap(roadmap_id)
+    response = self.class.get(api_url + "roadmaps/#{roadmap_id}", headers: {"authorization" => @auth_token})
+    @roadmap = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get(api_url + "checkpoints/#{checkpoint_id}", headers: {"authorization" => @auth_token})
+    @checkpoint = JSON.parse(response.body)
   end
 
 private
